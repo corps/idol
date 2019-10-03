416d11d660d863934a77c6c134facbc552c54f36
Zachary Collins <recursive.cookie.jar@gmail.com>
2019-10-02 18:53:29 -0700

    WIP

--- test/run_composition_tests.sh
+++ test/run_composition_tests.sh
@@ -10,12 +10,22 @@ for file in cases/composition/*; do
 
   output=`$idol -N actual/composition/ -- $file 2>&1`
   if test $? -ne 0; then
+    if ! test -e $expected.err; then
+      echo "$file composition failed unexpectedly!  Check $expected.err, output has been added there."
+      echo "$output" > $expected.err
+    fi
+
     if [[ "$output" != "$(cat $expected.err)" ]]; then
-      echo "$file composition failed unexpectedly!  Check expected/composition/$file.error, output was
+      echo "$file composition failed unexpectedly!  Check $expected.err, output was
 $output"
       exit 1
     fi
   else
+    if ! test -e $expected.json; then
+      echo "No $expected.json file found, creating out from output"
+      cat $actual.json | jq -S > $expected.json
+    fi
+
     if [[ "$(cat $expected.json | jq -S)" != "$(cat $actual.json | jq -S)" ]]; then
       echo "$file composition did not match expectation!"
       diff <(cat $expected.json | jq -S) <(cat $actual.json | jq -S)
