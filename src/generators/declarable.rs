5444a7cda266fba06daf680fb1536c6cadfb6b7a
Zach Collins <recursive.cookie.jar@gmail.com>
2020-04-07 23:26:16 -0700

    Finishing up things

--- src/generators/declarable.rs
+++ src/generators/declarable.rs
@@ -1,3 +1,18 @@
-pub trait Declarable<T> {
-    fn write(writer: &mut Writer) {}
+pub trait Scope {
+    type Declaration;
+    type Symbol;
+
+    fn write(self) -> Result<(), String>;
+    fn declare<T, D>(t: T) -> Result<Self::Declaration, String>
+    where
+        D: Declarable<Declaration = Self::Declaration, Symbol = Self::Symbol>,
+        T: Into<D>;
+}
+
+pub trait Declarable {
+    type Declaration;
+    type Symbol;
+
+    fn suggested_name() -> Self::Symbol;
+    fn named(name: Self::Symbol) -> Self::Declaration;
 }
--- src/generators/idol_rs.rs
+++ src/generators/idol_rs.rs
@@ -1,11 +1,46 @@
-// A thing should know where it is exported from, how to import it.
+use crate::models::schema::Module;
+use crate::modules_store::ModulesStore;
+use std::hash::Hash;
 
-// I have a type I want to process,
-// AMType { M - M }
+#[derive(PartialEq, PartialOrd, Hash, Ord, Eq)]
+pub struct RustSymbolName(String);
 
-// Type { type: ATYpe { super: Type { type: BType { type: None } } } }
-//
-//
-//
+#[derive(PartialEq, PartialOrd, Hash, Ord, Eq)]
+pub struct RustModuleNodeName(String);
 
-//
+impl<T: ToString> From<T> for RustModuleNodeName {
+    fn from(i: T) -> Self {
+        // TODO: rename invalid rust module names?
+        RustModuleNodeName(i.to_string())
+    }
+}
+
+#[derive(PartialEq, PartialOrd, Hash, Ord, Eq)]
+pub enum RustRootModuleNode {
+    ModSelf,
+    Crate,
+    Named(RustModuleNodeName),
+}
+
+#[derive(Hash, PartialOrd, PartialEq, Ord, Eq)]
+pub struct RustFileIdentifier(RustModuleNodeName, Vec<RustModuleNodeName>);
+pub type RustFilePart<T> = (RustFileIdentifier, T);
+
+impl From<&Module> for RustFileIdentifier {
+    fn from(module: &Module) -> Self {
+        let mut module_name_parts = module.module_name.split(".").into_iter();
+        let module_root = module_name_parts.next().expect("Moo");
+        RustFileIdentifier(
+            module_root.into(),
+            module_name_parts.map(|s| s.into()).collect(),
+        )
+    }
+}
+
+pub fn generate(store: &ModulesStore) -> Result<(), String> {
+    Ok(())
+}
+
+pub fn module_output(module: &Module) -> Vec<RustFilePart<()>> {
+    vec![(module.into(), ())]
+}
